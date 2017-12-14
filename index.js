d2a328f801b2a565626e4c39e47e76c7da05436a
Adriel Fabella <adriel.fab@gmail.com>
2017-12-13 23:55:33 -0500

    Fixing up server

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var path = require('path');
-
-app.get('/', function(req, res){
-  var express=require('express');
-  app.use(express.static(path.join(__dirname)));
-  res.sendFile(path.join(__dirname, '../adrielFab.github.io/submit', 'index.html'));
-});
 
 io.on('connection', function(socket){
   socket.on('teamInformation', function(teamName){
